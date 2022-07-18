cdca8900dd83a65b1f0c3c3173227ba67621ff1e
giovanniPepi <gtpepi@hotmail.com>
2022-07-18 16:01:53 -0300

    Simplify level grid creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
-import { HashRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Level from "./components/Levels";
+import Home from "./components/Home";
+import uniqid from "uniqid";
 
 const App = () => {
+  const [lvl, setLvl] = useState(0);
+  const possibleLvls = [1, 2, 3, 4, 5, 6];
+
   return (
     <HashRouter basename="/">
-      <Header />
-      <div>APP</div>
+      <Header lvl={lvl} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home lvl={lvl} setLvl={setLvl} possibleLvls={possibleLvls} />
+          }
+        />
+        {possibleLvls.map((item) => {
+          return (
+            <Route
+              key={uniqid()}
+              path={`/level/${item}`}
+              element={<Level lvl={item} setLvl={setLvl} />}
+            />
+          );
+        })}
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>Route not found!</p>
+            </main>
+          }
+        />
+      </Routes>
     </HashRouter>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import uniqid from "uniqid";
 import { Link } from "react-router-dom";
 import Level from "./Levels";
 
-const Home = ({ lvl, setLvl }) => {
+const Home = ({ lvl, setLvl, possibleLvls }) => {
   const handleLevel = (n) => {
     setLvl(n);
   };
@@ -12,54 +12,18 @@ const Home = ({ lvl, setLvl }) => {
     <main className="home">
       <section className="levelGrid">
         <p>Select level:</p>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(1)}
-          to={`/level/${1}`}
-          element={<Level lvl={1} setLvl={setLvl} />}
-        >
-          Level 1
-        </Link>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(2)}
-          to={`/level/${2}`}
-          element={<Level lvl={2} setLvl={setLvl} />}
-        >
-          Level 2
-        </Link>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(3)}
-          to={`/level/${3}`}
-          element={<Level lvl={3} setLvl={setLvl} />}
-        >
-          Level 3
-        </Link>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(4)}
-          to={`/level/${4}`}
-          element={<Level lvl={4} setLvl={setLvl} />}
-        >
-          Level 4
-        </Link>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(5)}
-          to={`/level/${5}`}
-          element={<Level lvl={5} setLvl={setLvl} />}
-        >
-          Level 5
-        </Link>
-        <Link
-          key={uniqid()}
-          onClick={() => handleLevel(6)}
-          to={`/level/${6}`}
-          element={<Level lvl={6} setLvl={setLvl} />}
-        >
-          Level 6
-        </Link>
+        {possibleLvls.map((item) => {
+          return (
+            <Link
+              key={uniqid()}
+              onClick={() => handleLevel(item)}
+              to={`/level/${item}`}
+              element={<Level lvl={item} setLvl={setLvl} />}
+            >
+              Level {item}
+            </Link>
+          );
+        })}
       </section>
     </main>
   );
