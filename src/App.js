1cb3b7cfbd1451cc3406065804af9c8a5dac468e
giovanniPepi <gtpepi@hotmail.com>
2022-08-01 08:52:25 -0300

    Add useTimer custom hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Level from "./components/Levels";
