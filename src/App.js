42616bf7c8cdf63fd222be094da44265a69f3cb3
giovanniPepi <gtpepi@hotmail.com>
2022-08-02 13:51:54 -0300

    Move game logic from Levels to App. Move databases to state. Add initial check for gameover

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
@@ -15,30 +15,222 @@ import waldoMini from "../src/img/waldoAvatar.jpg";
 import odlawMini from "../src/img/OdlawAvatar.jpg";
 import wendaMini from "../src/img/wenda.jpg";
 import wizardMini from "../src/img/wizardAvatar.jpg";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const App = () => {
   const [lvl, setLvl] = useState(1);
   const [inHome, setInHome] = useState(false);
   const possibleLvls = [1, 2, 3, 4, 5, 6];
   const [chosenCharacter, setChosenCharacter] = useState("default");
-  const [foundCharacters, setFoundCharacters] = useState([""]);
+  const [foundCharacters, setFoundCharacters] = useState([]);
   const [time, setTime] = useState(0);
-
-  const imgDatabase = [
-    { photo: bg1, waldo: true, odlaw: true, wenda: false, wizard: true },
-    { photo: bg2, waldo: true, odlaw: false, wenda: false, wizard: false },
-    { photo: bg3, waldo: true, odlaw: true, wenda: true, wizard: true },
-    { photo: bg4, waldo: true, odlaw: true, wenda: true, wizard: false },
-    { photo: bg5, waldo: true, odlaw: true, wenda: true, wizard: true },
-    { photo: bg6, waldo: true, odlaw: false, wenda: false, wizard: false },
-  ];
-
-  const avatarDatabase = {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [show, setShow] = useState(false);
+  const [clickLocation, setClickLocation] = useState({
+    left: "0%",
+    top: "0%",
+  });
+  const [imgDatabase] = useState([
+    {
+      photo: bg1,
+      waldo: true,
+      odlaw: true,
+      wenda: false,
+      wizard: true,
+      quantity: 3,
+    },
+    {
+      photo: bg2,
+      waldo: true,
+      odlaw: false,
+      wenda: false,
+      wizard: false,
+      quantity: 1,
+    },
+    {
+      photo: bg3,
+      waldo: true,
+      odlaw: true,
+      wenda: true,
+      wizard: true,
+      quantity: 4,
+    },
+    {
+      photo: bg4,
+      waldo: true,
+      odlaw: true,
+      wenda: true,
+      wizard: false,
+      quantity: 3,
+    },
+    {
+      photo: bg5,
+      waldo: true,
+      odlaw: true,
+      wenda: true,
+      wizard: true,
+      quantity: 4,
+    },
+    {
+      photo: bg6,
+      waldo: true,
+      odlaw: false,
+      wenda: false,
+      wizard: false,
+      quantity: 1,
+    },
+  ]);
+  const [avatarDatabase] = useState({
     waldo: waldoMini,
     odlaw: odlawMini,
     wenda: wendaMini,
     wizard: wizardMini,
+  });
+
+  const getImgLocation = (e) => {
+    // nativeEvent acess JS property inside the React wrapper
+    const x = Math.round(
+      (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
+    );
+    const y = Math.round(
+      (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
+    );
+    const coords = { x, y };
+    return coords;
   };
+
+  const imageClick = (e) => {
+    const newCoords = getImgLocation(e);
+    setCoords(newCoords);
+    handleClickLocation(newCoords);
+    setShow(true);
+  };
+
+  const handleClickLocation = (coords) => {
+    const { x, y } = coords;
+    const updatedCoords = { left: x + "%", top: y + "%" };
+    setClickLocation(updatedCoords);
+  };
+
+  useEffect(() => {
+    // Firebase
+    const colRef = collection(db, "coords");
+
+    const coordList = [];
+
+    const getCoordsFromFirestore = async (character, lvl) => {
+      const docs = await getDocs(colRef);
+      docs.forEach((doc) => {
+        coordList.push({ ...doc.data() });
+      });
+      switch (character) {
+        case "waldo":
+          const waldoPosition = {
+            x: coordList[0]["waldo"][lvl]["x"],
+            y: coordList[0]["waldo"][lvl]["y"],
+          };
+          return waldoPosition;
+        case "wenda":
+          const wendaPosition = {
+            x: coordList[0]["wenda"][lvl]["x"],
+            y: coordList[0]["wenda"][lvl]["y"],
+          };
+          return wendaPosition;
+        case "wizard":
+          const wizardPosition = {
+            x: coordList[0]["wizard"][lvl]["x"],
+            y: coordList[0]["wizard"][lvl]["y"],
+          };
+          return wizardPosition;
+        case "odlaw":
+          const odlawPosition = {
+            x: coordList[0]["odlaw"][lvl]["x"],
+            y: coordList[0]["odlaw"][lvl]["y"],
+          };
+          return odlawPosition;
+        default:
+          const defaultPosition = {
+            x: coordList[0]["default"][lvl]["x"],
+            y: coordList[0]["default"][lvl]["y"],
+          };
+          console.log("default or char not found");
+          return defaultPosition;
+      }
+    };
+
+    const validateChosenCoords = async (chosenCharacter, lvl) => {
+      // avoid unecessary processing in the first load
+      if (chosenCharacter === "default") return;
+
+      const serverCoords = await getCoordsFromFirestore(
+        chosenCharacter,
+        lvl - 1
+      );
+
+      const xPair = [coords["x"], serverCoords["x"]];
+      const yPair = [coords["y"], serverCoords["y"]];
+
+      // logic to search for coords nearby, since a character may occupy serveral coords
+      const winConditionX =
+        xPair[0] === xPair[1] ||
+        xPair[0] + 1 === xPair[1] ||
+        xPair[0] + 2 === xPair[1] ||
+        xPair[0] - 1 === xPair[1] ||
+        xPair[0] - 2 === xPair[1];
+
+      const winConditionY =
+        yPair[0] === yPair[1] ||
+        yPair[0] + 1 === yPair[1] ||
+        yPair[0] + 2 === yPair[1] ||
+        yPair[0] + 3 === yPair[1] ||
+        yPair[0] - 1 === yPair[1] ||
+        yPair[0] - 2 === yPair[1] ||
+        yPair[0] - 3 === yPair[1];
+
+      console.log(
+        "clicked: ",
+        coords,
+        "target: ",
+        serverCoords,
+        "conditions summary: ",
+        "winconditionx: ",
+        winConditionX,
+        "winconditionY",
+        winConditionY
+      );
+
+      if (winConditionX && winConditionY) {
+        // avoids finding the same character
+        if (foundCharacters.includes(chosenCharacter)) {
+          console.log("alreayd found ", chosenCharacter);
+          return;
+        }
+
+        alert(`you've found ${chosenCharacter}`);
+
+        setFoundCharacters((foundCharacters) => [
+          ...foundCharacters,
+          chosenCharacter,
+        ]);
+      } else console.log("wincondition not met!");
+    };
+
+    // will run each time the useEffect runs
+    validateChosenCoords(chosenCharacter, lvl);
+
+    const checkGameOver = () => {
+      if (foundCharacters.length === imgDatabase[lvl - 1]["quantity"]) {
+        console.log("game over");
+      }
+    };
+
+    checkGameOver();
+
+    // resets chosen char
+    setChosenCharacter("default");
+  }, [chosenCharacter, lvl, coords, foundCharacters, imgDatabase]);
 
   return (
     <HashRouter basename="/">
@@ -76,15 +268,19 @@ const App = () => {
               element={
                 <Level
                   lvl={item}
-                  setLvl={setLvl}
                   imgDatabase={imgDatabase[item - 1]}
                   avatarDatabase={avatarDatabase}
-                  inHome={inHome}
                   setInHome={setInHome}
                   chosenCharacter={chosenCharacter}
                   setChosenCharacter={setChosenCharacter}
                   foundCharacters={foundCharacters}
                   setFoundCharacters={setFoundCharacters}
+                  isGameOver={isGameOver}
+                  setIsGameOver={setIsGameOver}
+                  show={show}
+                  setShow={setShow}
+                  clickLocation={clickLocation}
+                  imageClick={imageClick}
                 />
               }
             />
--- src/components/Levels.js
+++ src/components/Levels.js
@@ -1,156 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
+
 import DropdownMenu from "./DropdownMenu";
-import { db } from "../firebase";
 
 const Level = ({
   lvl,
   imgDatabase,
   avatarDatabase,
-  inHome,
   setInHome,
   chosenCharacter,
   setChosenCharacter,
   foundCharacters,
   setFoundCharacters,
+  isGameOver,
+  setIsGameOver,
+  show,
+  setShow,
+  clickLocation,
+  imageClick,
 }) => {
-  const [coords, setCoords] = useState({ x: 0, y: 0 });
-  const [show, setShow] = useState(false);
-  const [clickLocation, setClickLocation] = useState({ left: "0%", top: "0%" });
-
   useEffect(() => {
     // handles show home
     setInHome(false);
-
-    // Firebase
-    const colRef = collection(db, "coords");
-
-    const coordList = [];
-
-    const getCoordsFromFirestore = async (character, lvl) => {
-      const docs = await getDocs(colRef);
-      docs.forEach((doc) => {
-        coordList.push({ ...doc.data() });
-      });
-      switch (character) {
-        case "waldo":
-          const waldoPosition = {
-            x: coordList[0]["waldo"][lvl]["x"],
-            y: coordList[0]["waldo"][lvl]["y"],
-          };
-          return waldoPosition;
-        case "wenda":
-          const wendaPosition = {
-            x: coordList[0]["wenda"][lvl]["x"],
-            y: coordList[0]["wenda"][lvl]["y"],
-          };
-          return wendaPosition;
-        case "wizard":
-          const wizardPosition = {
-            x: coordList[0]["wizard"][lvl]["x"],
-            y: coordList[0]["wizard"][lvl]["y"],
-          };
-          return wizardPosition;
-        case "odlaw":
-          const odlawPosition = {
-            x: coordList[0]["odlaw"][lvl]["x"],
-            y: coordList[0]["odlaw"][lvl]["y"],
-          };
-          return odlawPosition;
-        default:
-          const defaultPosition = {
-            x: coordList[0]["default"][lvl]["x"],
-            y: coordList[0]["default"][lvl]["y"],
-          };
-          console.log("default or char not found");
-          return defaultPosition;
-      }
-    };
-
-    const validateChosenCoords = async (chosenCharacter, lvl) => {
-      // avoid unecessary processing in the first load
-      if (chosenCharacter === "default") return;
-
-      const serverCoords = await getCoordsFromFirestore(
-        chosenCharacter,
-        lvl - 1
-      );
-
-      const xPair = [coords["x"], serverCoords["x"]];
-      const yPair = [coords["y"], serverCoords["y"]];
-
-      // logic to search for coords nearby, since a character may occupy serveral coords
-      const winConditionX =
-        xPair[0] === xPair[1] ||
-        xPair[0] + 1 === xPair[1] ||
-        xPair[0] + 2 === xPair[1] ||
-        xPair[0] - 1 === xPair[1] ||
-        xPair[0] - 2 === xPair[1];
-
-      const winConditionY =
-        yPair[0] === yPair[1] ||
-        yPair[0] + 1 === yPair[1] ||
-        yPair[0] + 2 === yPair[1] ||
-        yPair[0] + 3 === yPair[1] ||
-        yPair[0] - 1 === yPair[1] ||
-        yPair[0] - 2 === yPair[1] ||
-        yPair[0] - 3 === yPair[1];
-
-      console.log(
-        "clicked: ",
-        coords,
-        "target: ",
-        serverCoords,
-        "conditions summary: ",
-        "winconditionx: ",
-        winConditionX,
-        "winconditionY",
-        winConditionY
-      );
-
-      if (winConditionX && winConditionY) {
-        if (foundCharacters.includes(chosenCharacter)) {
-          console.log("alreayd found ", chosenCharacter);
-          return;
-        }
-        setFoundCharacters((foundCharacters) => [
-          ...foundCharacters,
-          chosenCharacter,
-        ]);
-
-        alert(`you've found ${chosenCharacter}`);
-      } else console.log("wincondition not met!");
-    };
-    // will run each time the useEffect runs
-    validateChosenCoords(chosenCharacter, lvl);
-    // resets chosen char
-    setChosenCharacter("default");
   });
-
-  const getImgLocation = (e) => {
-    // nativeEvent acess JS property inside the React wrapper
-    const x = Math.round(
-      (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
-    );
-    const y = Math.round(
-      (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
-    );
-    const coords = { x, y };
-    return coords;
-  };
-
-  const imageClick = (e) => {
-    const newCoords = getImgLocation(e);
-    setCoords(newCoords);
-    handleClickLocation(newCoords);
-    setShow(true);
-  };
-
-  const handleClickLocation = (coords) => {
-    const { x, y } = coords;
-    const updatedCoords = { left: x + "%", top: y + "%" };
-    setClickLocation(updatedCoords);
-  };
 
   return (
     <section className="waldoLvl">
