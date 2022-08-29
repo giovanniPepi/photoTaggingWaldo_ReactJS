837ebb7be5ad96f4acbba92881149cd448741e99
a4 <gtpepi@hotmail.com>
2022-08-28 22:15:38 -0300

    Add mapping through higshcores on <Highscores /> to sort through all highscores received

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -5,13 +5,14 @@ const Highscores = ({ highscores }) => {
     <>
       <h2 className="highscoresTitle">Highscores</h2>
       <section className="highscoreContainer">
-        <h3>Level 01: </h3>
         {highscores.map((item) => {
-          return (
-            <p key={uniqid()} className="highscoreItem">
-              User {item.userName} - {item.time} s
-            </p>
-          );
+          return item.map((subItem) => {
+            return (
+              <p key={uniqid()} className="highscoreItem">
+                User {subItem.userName} - {subItem.time} s
+              </p>
+            );
+          });
         })}
       </section>
     </>
