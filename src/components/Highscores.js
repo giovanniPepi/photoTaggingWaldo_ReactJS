05b605b05ebd9631226459119d389a1455dc9d58
a4 <gtpepi@hotmail.com>
2022-08-29 10:53:53 -0300

    Update highscores to display on ascending time order for each level

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,11 +1,22 @@
 import uniqid from "uniqid";
 
 const Highscores = ({ highscores }) => {
+  const compareTime = (a, b) => {
+    console.log("compared");
+    return a.time > b.time ? b : a;
+  };
   return (
     <>
       <h2 className="highscoresTitle">Highscores</h2>
       <section className="highscoreContainer">
+        {/* Each array item is a level*/}
         {highscores.map((item) => {
+          /* each subItem is a player object   */
+
+          item.sort((a, b) => {
+            return a.time - b.time;
+          });
+          console.log(item);
           return item.map((subItem) => {
             return (
               <p key={uniqid()} className="highscoreItem">
